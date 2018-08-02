8098bfb53ce9a31cd8cd470d2d42a96a8f1a4b65
reworks <dev.dom.re@gmail.com>
2018-08-02 10:34:48 +0800

    missing docs

--- src/Font.cpp
+++ src/Font.cpp
@@ -13,12 +13,14 @@ Font::Font()
 
 void Font::create(const char* file, int size, int style)
 {
+	// Create font and then set its style.
 	m_font = TTF_OpenFont(file, size);
 	TTF_SetFontStyle(m_font, style);
 }
 
 void Font::destroy()
 {
+	// Clean up font.
 	TTF_CloseFont(m_font);
 }
 
--- src/Fonts.cpp
+++ src/Fonts.cpp
@@ -8,6 +8,7 @@
 
 Fonts::Fonts(const char* regularFile, const char* boldFile, const char* italicFile, int size)
 {
+	// Create stored fonts.
 	m_regularFont.create(regularFile, size, TTF_STYLE_NORMAL);
 	m_boldFont.create(boldFile, size, TTF_STYLE_BOLD);
 	m_italicFont.create(italicFile, size, TTF_STYLE_ITALIC);
@@ -15,6 +16,7 @@ Fonts::Fonts(const char* regularFile, const char* boldFile, const char* italicFi
 
 Fonts::~Fonts()
 {
+	// Clean up stored fonts.
 	m_regularFont.destroy();
 	m_boldFont.destroy();
 	m_italicFont.destroy();
